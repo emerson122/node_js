26bb4ac36bb58d3877a05c0b55580376ccada30b
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-08-29 20:50:53 -0600

    Add files via upload

---
+++ Pagina web/index.js
@@ -0,0 +1,34 @@
+/*
+// para instalar estos modulos ejecutar el (npm install )
+const http = require('http');
+const colors = require('colors');
+const arrancarserver = function(req,res){
+    res.writeHead(200,{'content-type': 'text/html'});
+    res.write('<h1> funciona we</h1>');
+    res.end;
+}
+const server=http.createServer(arrancarserver);
+//instalar colores con (npm install colors)
+server.listen(3000,function(){
+    console.log('El sevidor se ejecuto correctamente en el puerto 3000'.green)
+    console.log('Ve al navegador en localhost 3000'.underline.green)
+});
+
+//npm crear package json con los recursos utilizados
+
+*/
+
+const express = require('express');
+const colors = require('colors');
+const server = express();
+
+server.get('/', function(req,res){
+    res.send('hola desde express');
+    res.end;
+});
+
+// => siginifica function
+server.listen(3000, ()=>{
+    console.log('El sevidor se ejecuto correctamente en el puerto 3000'.green)
+    console.log('Ve al navegador en localhost 3000'.underline.green)
+});
